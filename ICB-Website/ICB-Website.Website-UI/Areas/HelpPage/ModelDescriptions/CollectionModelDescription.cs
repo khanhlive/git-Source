a3d3eb9be7f9ac9c3fa78ec7da32394545461c87
khanhlive <khanhngdinh@outlook.com>
2017-08-22 16:21:04 +0700

    add with ICB-Website

---
+++ ICB-Website/ICB-Website.Website-UI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ICB_Website.Website_UI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ICB-Website/ICB-Website.Website-UI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ICB_Website.Website_UI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
